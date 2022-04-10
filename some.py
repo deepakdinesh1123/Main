e02392547cb6ce27a9799789442050d7b909b751
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-04-10 11:04:20 +0530

    flake8 checks resolved

--- some.py
+++ some.py
@@ -1,6 +1,5 @@
 
 from django.db import models
-# importing validationerror
 from django.core.exceptions import ValidationError
 
 
@@ -38,17 +37,5 @@ jobs:
         run: bash ./gradlew lintDebug --stacktrace
 """)
 print("self hosted runner added(ubuntu vm) and windows removed")
- 
-# creating a validator function
-def validate_geeks_mail(value):
-    if "@gmail.com" in value:
-        return value
-    else:
-        raise ValidationError("This field accepts mail id of google only")
- 
- 
-# Create your models here.
-class GeeksModel(models.Model):
-    geeks_mail = models.CharField(max_length = 200)
-  
-print("run checks")
+a = ValidationError()
+b = models.Model()
